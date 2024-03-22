bba495ab65c09a414f36c9e182d0b7606593663b
Kota Suzuki <addressuzuki@gmail.com>
2024-03-22 23:33:19 +0900

    Initial Commit

---
+++ ReadingLogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReadingLogs
+//
+//  Created by 鈴木航太 on 2024/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReadingLogs/ReadingLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingLogsApp.swift
+//  ReadingLogs
+//
+//  Created by 鈴木航太 on 2024/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadingLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
